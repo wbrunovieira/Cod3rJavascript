e9fe336ec079039addb6a4d2ff2e3fe8bd5fbf9c
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-04 09:57:57 +0100

    Added lessons basic functions, var and let

---
+++ Fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,5 @@
+console.log(Object)
+
+class Produto{}
+
+console.log(typeof Produto)
---
+++ Fundamentos/usandoLet1.js
@@ -0,0 +1,6 @@
+var numero = 10
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
