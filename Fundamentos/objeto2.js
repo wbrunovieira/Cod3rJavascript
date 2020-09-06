93cad5b84e81c770e5ced1c6a63f3ac21d99fe94
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-06 21:19:29 +0100

    added using var, hoisting, using var and let in loops

---
+++ Fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof object)
+console.log(typeof new object)
+
+const Cliente = function () {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente )
+
+class Produto {}
+console.log(typeof Produto)
+console.log(typeof new Produto)
