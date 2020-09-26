5aa570b250215b99ecabd2ad9c9586f5f47ab890
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-26 09:47:25 +0100

    created a api rest with CRUD, and tested all function in Postman

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,14 +2,49 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+// app.get('/produtos', (req, res,next) => {
+//     console.log('Middleware 1...')
+//     next()
+// })
+
+// app.get('/produtos', (req, res,next) => {
+//     res.send({ Nome: 'Notebook', preco: 1245.90}) // convert to json
+// })
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res,next) => {
-    console.log('Middleware 1...')
-    next()
+    res.send(bancoDeDados.getProdutos())
 })
 
-app.get('/produtos', (req, res,next) => {
-    res.send({ Nome: 'Notebook', preco: 1245.90}) // convert to json
+app.get('/produtos/:id',(req, res,next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos/', (req, res,next)=> {
+    const produto = bancoDeDados.salvarProduto({
+        
+        nome: req.body.nome,
+        preco: req.body.preco,
+    })
+    res.send(produto) //JSON
+})
+
+app.put('/produtos/:id', (req, res,next)=> {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco,
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res,next)=> {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
