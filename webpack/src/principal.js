b850501fa0faf584c21f101c634d1c16ba2582d8
wbrunovieira <wbrunovieira77@gmail.com>
2020-12-03 10:06:51 +0000

    learned how to export css, sass with web-pack

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,6 @@
-const Pessoa = require('.pessoa')
-
-import Pessoa from '.pessoa'
+import Pessoa from './pessoa'
+import './modulos/moduloA'
+import './assets'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,29 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //adciona CSS a DOM injetando a tag <style>
+                'css-loader', //interpreta #import, url()...
+                'sass-loader',
+            ]
+        }]
+    }
     
 }
